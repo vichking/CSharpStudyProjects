d89ad55e54d329e890d90b528c68d46cae9ed38f
Victoria <vichking@gmail.com>
2018-06-08 20:08:56 +0300

    Delegates study

---
+++ DelegatesStudyProjects/DelegatesStudyProjects/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesStudyProjects
+{
+    delegate string SignActionHandler(string subject);
+
+    class Person
+    {
+        public event SignActionHandler SignActions;
+
+        public string FullName { get; set; }
+        public bool WantToSign { get; set; }
+
+        public Person(string fullName, bool wantToSign)
+        {
+            FullName = fullName;
+            WantToSign = wantToSign;
+        }
+        
+        public string SignAction()
+        {
+            if (!WantToSign || SignActions == null)
+                return null;
+            return SignActions.Invoke(FullName);
+        }
+    }
+}
